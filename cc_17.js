4aab57a610c33e705f71d35d267e387c5da25a2a
Sophia Dolan <25srdolan@gmail.com>
2025-03-27 21:55:17 -0400

    Task 1 New customer creation and total spent after purchases.

--- cc_17.js
+++ cc_17.js
@@ -1,13 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <title>Product Dashboard</title>
-  <link rel="stylesheet" href="style.css" />
-</head>
-<body>
-  <h1>Store Products</h1>
-  <div id="product-container"></div>
-  <script src="cc_17.js"></script>
-</body>
-</html>
+//task 1 :New customer creation and total spent after purchases.
+class Customer {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+        this.purchaseHistory = [];
+    }
+
+    addPurchase(amount) {
+        this.purchaseHistory.push(amount);
+        console.log(`${this.name} purchased $${amount}`);
+    }
+
+    getTotalSpent() {
+        const total = this.purchaseHistory.reduce((sum, amount) => sum + amount, 0);
+        console.log(`${this.name} spent $${total}`);
+        return total;
+    }
+}
+
+const customer1 = new Customer("Batman", "batmobile@gmail.com");
+customer1.addPurchase(20);
+customer1.addPurchase(15);
+customer1.getTotalSpent();
